4c0b20592bee9cc2a82e1ca3d85249fc7e8ecb88
jayseryu <dgkim723@hotmail.com>
2018-06-07 16:29:42 +0900

    Render layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -27,7 +27,7 @@
     <!-- Main Context on video wrap -->
     <div class="main_title" >
       <h2>OPEN MIND</h2>
-      <h3>우리는 개방적입니다.</h3>
+      <h3>우리는 개방적입니다</h3>
       <div class="division_bar"></div>
       <p>복층으로 이루어진 색다른 회사 공간에서</p>
       <p>우리는 서로의 의견을 자유롭게 이야기하고, 각 부서간 원활하게 소통하며 일합니다.</p>
@@ -36,141 +36,140 @@
       </div>
     </div>
 
-    <section class="section_1">
-      <p>엘씨 벤처스는</p>
-      <h2>감성과 기술이 만납니다.</h2>
-      <p>크리에이티브한 엘씨 벤처스는 브랜딩과 웹사이트 및 이커머스 솔루션</p>
-      <p>그리고 다양한 미디어 컨텐츠를 포함한 통합 마케팅 솔루션을 제공하는 디지털 에이전시입니다.</p>
-      <br/>
-      <p>사람의 감성을 닮은 디자인에 디지털 기술을 더함으로써</p>
-      <p>고객의 비즈니스에 필요한 아름답고도 유용한 최신의 크리에이티브 솔루션을 한발 앞서 제공합니다.</p>
+    <section class="section_1"><!--make ul list-->
+
+      <div class="culture">Culture</div>
+
+      <div class="culture_wrap">
+        <div class="culture_img_wrap">
+          <div class="culture_img">
+            <ul>
+              <li class="li_1"></li>
+              <li class="li_2" style="background-color: darkblue"></li>
+            </ul>
+            <div class="img_nav">
+              <button class="prev_button">
+                <img src="../assets/pages/home/arrow_left.png" alt="Left button">
+              </button>
+              <button class="next_button">
+                <img src="../assets/pages/home/arrow_right.png" alt="Right button">
+              </button>
+            </div>
+            <div class="img_position">
+              <p>1/10</p>
+            </div>
+          </div>
+          <div class="img_border"></div>
+        </div>
+        <div class="culture_text">
+          <div class="inner_title">
+            <h4>RESPECT</h4>
+            <p>우리는 존중합니다</p>
+          </div>
+          <div class="division_bar_small"></div>
+          <div class="inner_content">
+            <p>나이와 직급에 상관없이</p>
+            <p>모두에게 존댓말과 높임말을 사용합니다.</p>
+            <p>동등한 위치에서 자신의 생각을 말하지만,</p>
+            <p>스스로가 내린 결정의 책임감을 가지고 일합니다.</p>
+          </div>
+        </div>
+        <div class="side_text">
+          <p>Have sense of responsibility</p>
+        </div>
+      </div>
+
     </section>
     <!--section 1-->
 
     <section class="section_2">
-      <div>
-        <p class="section_2_title">엘씨 벤처스가 하는 일</p>
-        <h3 class="section_2_describe">우리의 비지니스는 새로움에 대한 도전입니다.</h3>
-        <div>
-          <ul class="what_we_doing">
-            <li class="doing_list">
-              <p class="doing_list_title">웹사이트</p>
-              <p>
-                우리는 사용자 경험을 통해 브랜드를 이야기합니다.
-                사용자에게 초점을 맞추고 고객이 원하는 브랜드의 고유한 정체성을 전달하며
-                경쟁력있는 웹사이트를 구축하기 위해 끊임없이 고민합니다.
-              </p>
-              <ul class="doing_ul">
-                <li>UX/UI</li>
-                <li>마이크로 사이트</li>
-                <li>웹 표준화</li>
-                <li>반응형 웹</li>
-              </ul>
-            </li>
 
-            <li class="doing_list">
-              <p class="doing_list_title">E-커머스</p>
-              <p>
-                E-커머스는 브랜드와 고객을 연결합니다.
-                소비자가 원하는 정보는 쉽게, 개인 결제 정보는 안전하도록 설계하여
-                고객의 매출 증대에 기여합니다.
-              </p>
-              <ul class="doing_ul">
-                <li>온라인 쇼핑몰</li>
-                <li>모바일 앱</li>
-                <li>프로모션</li>
-                <li>인터페이스 디자인</li>
-              </ul>
-            </li>
+      <div class="culture_wrap">
+        <div class="culture_img_wrap">
+          <div class="culture_img">
+            <ul>
+              <li class="li_1"></li>
+              <li class="li_2" style="background-color: darkcyan"></li>
+            </ul>
+            <div class="img_nav">
+              <button class="prev_button">
+                <img src="../assets/pages/home/arrow_left.png" alt="Left button">
+              </button>
+              <button class="next_button">
+                <img src="../assets/pages/home/arrow_right.png" alt="Right button">
+              </button>
+            </div>
+            <div class="img_position">
+              <p>1/10</p>
+            </div>
+          </div>
+          <div class="img_border"></div>
+        </div>
+        <div class="culture_text">
+          <div class="inner_title">
+            <h4>ENJOY</h4>
+            <p>우리는 즐겁습니다</p>
+          </div>
+          <div class="division_bar_small"></div>
+          <div class="inner_content">
+            <p>날이 좋은 날에는,</p>
+            <p>직원들과 함께 즉흥적으로 소풍을 떠납니다.</p>
+            <p>벚꽃이 만개할 때는, 다같이 벚꽅놀이를 갑니다.</p>
+            <p>연말에는 송년회를 열어</p>
+            <p>한 해의 격려와 응원을 보내줍니다.</p>
+          </div>
+        </div>
+        <div class="side_text">
+          <p>Give encouragement to each other</p>
+        </div>
+      </div>
 
-            <li class="doing_list">
-              <p class="doing_list_title">브랜딩</p>
-              <p>
-                브랜드는 소비자와의 관계입니다.
-                브랜딩 전략에 따른 명확하면서도 혁신적인 디자인 아이덴티티를 구현하여
-                소비자 기반의 브랜드 자산가치를 높입니다.
-              </p>
-              <ul class="doing_ul">
-                <li>홈페이지</li>
-                <li>BI/CI</li>
-                <li>홍보영상</li>
-              </ul>
-            </li>
-
-            <li class="doing_list">
-              <p class="doing_list_title">마케팅</p>
-              <p>
-                고객과 우리의 목표는 같습니다.
-                고객의 니즈와 시장에 대한 깊은 통찰력으로 새로운 트렌드를 끌어내어
-                우리의 고객은 ‘다르다.’는 차별화된 메시지를 소비자에게 명확하게 전달합니다.
-              </p>
-              <ul class="doing_ul">
-                <li>온라인 프로모션</li>
-                <li>디지털 마케팅</li>
-                <li>컨텐츠 마케팅</li>
-              </ul>
-            </li>
-
-            <li class="doing_list">
-              <p class="doing_list_title">모바일</p>
-              <p>
-                스마트폰은 이제 일상이 되었습니다.
-                장소, 환경에 구애받지 않고
-                언제 어디서든 이용이 가능한 모바일웹 구현으로 PC와 동일한 경험을 제공합니다.
-              </p>
-              <ul class="doing_ul">
-                <li>모바일 웹</li>
-                <li>모바일 웹</li>
-                <li>M 커머스</li>
-                <li>하이브리드 앱</li>
-              </ul>
-            </li>
-
-            <li class="doing_list">
-              <p class="doing_list_title">미디어</p>
-              <p>
-                훌륭한 경험은 매체를 가리지 않습니다.
-                영상, 전시, 광고 등 다양한 미디어에서
-                차별화된 크리에이티브로 마음을 움직이는 디자인을 추구합니다.
-              </p>
-              <ul class="doing_ul">
-                <li>모션그래픽</li>
-                <li>바이럴</li>
-                <li>전시영상</li>
-                <li>인포그래픽</li>
-              </ul>
-            </li>
-          </ul>
-        </div><!--what we doing wrap-->
-      </div><!--bind all in section-->
     </section>
     <!--section 2-->
 
     <section class="section_3">
-      <div class="section_3_boxes">
-        <div class="meet_box_1">
-          <h3 class="box_title">엘씨 사람들</h3>
-          <h4 class="box_describe">팀을 만나보세요.</h4>
-          <div class="box_contents">
-            <p>기획, 디자이너, 개발자 등 각 분야에서</p>
-            <p>탁월한 엘씨벤처스의 팀을 소개합니다.</p>
+
+      <div class="culture_wrap">
+        <div class="culture_img_wrap">
+          <div class="culture_img">
+            <ul>
+              <li class="li_1"></li>
+              <li class="li_2"></li>
+            </ul>
+            <div class="img_nav">
+              <button class="prev_button">
+                <img src="../assets/pages/home/arrow_left.png" alt="Left button">
+              </button>
+              <button class="next_button">
+                <img src="../assets/pages/home/arrow_right.png" alt="Right button">
+              </button>
+            </div>
+            <div class="img_position">
+              <p>1/10</p>
+            </div>
           </div>
-          <div class="router_wrap">
-            <router-link class="router" to="/people">더 알아보기 &#62</router-link>
+          <div class="img_border"></div>
+        </div>
+        <div class="culture_text">
+          <div class="inner_title">
+            <h4>FREEDOM</h4>
+            <p>우리는 자유롭습니다</p>
+          </div>
+          <div class="division_bar_small"></div>
+          <div class="inner_content">
+            <p>공식 업무 시간은 오전 9시30분으로</p>
+            <p>여유롭게 아침을 시작합니다.</p>
+            <p>업무 중 피곤함이 몰려올 때는,</p>
+            <p>잠깐의 티타임으로 에너지를 얻습니다.</p>
+            <p>우리는 매년 하계 휴가를 통해</p>
+            <p>재충전의 시간을 보냅니다.</p>
           </div>
         </div>
-        <div class="meet_box_2">
-          <h3 class="box_title">엘씨 사무실</h3>
-          <h4 class="box_describe">사무실을 소개합니다.</h4>
-          <div class="box_contents">
-            <p>멋진 카페들이 많은 서울숲에 있는 엘씨벤처스는</p>
-            <p>자유롭고 크리에이티브한 공간입니다.</p>
-          </div>
-          <div class="router_wrap">
-            <router-link class="router" to="/contact">더 알아보기 &#62</router-link>
-          </div>
+        <div class="side_text">
+          <p>Have time of recharge</p>
         </div>
-      </div><!--boxes-->
+      </div>
+
     </section>
     <!--section 3-->
 
@@ -280,12 +279,14 @@
     word-break: keep-all;
     white-space: pre-line;
     overflow: hidden;
+    font-family: Impact, sans-serif;
     h2 {
       font-size: 55px;
       font-weight: bold;
       margin: 0;
     }
     h3 {
+      font-family: 'NanumSquare', sans-serif;
       font-size: 30px;
       font-weight: normal;
       margin: 0;
@@ -297,9 +298,10 @@
       margin: 12px auto 20px;
     }
     p {
+      font-family: 'NanumSquare', sans-serif;
       font-size: 16px;
     }
-  }
+  }/* Main title area end */
 
   /* Change Text size belongs to Web width */
   @media (max-width: 650px) {
@@ -318,130 +320,240 @@
 
   /* Section Part */
 
-  /* section 1 start */
-  .section_1 {
+  .section_1, .section_2, .section_3 {
     width: 100%;
-    padding: 50px 20px;
-    background-color: #efefef;
+    padding: 50px 0;
+    background-color: #ffffff;
     text-align: center;
     color: #515151;
+    font-family: Impact, sans-serif;
+  }
+
+  .culture {
+    width: 100px;
+    height: 25px;
+    line-height: 25px;
+    font-weight: bold;
+    color: #fefefe;
+    background-color: darkblue;
+    margin: 0 auto 40px;
+  }
+
+  .culture_wrap {
+    width: 100%;
+    max-width: 1200px;
+    padding: 0 20px;
+    margin: 0 auto;
+    text-align: center;
+    &:after {
+      content: '';
+      clear: both;
+      display: block;
+    }
+  }
+
+  .culture_img_wrap {
+    width: 60%;
+    text-align: center;
+    float: left;
+    &:after {
+      content: '';
+      clear: both;
+      display: block;
+    }
+    .culture_img {
+      position: relative;
+      width: 85%;
+      height: 0;
+      padding-bottom: 83%;
+      border-radius: 120%;
+      overflow: hidden;
+      float: left;
+      z-index: 400;
+      border: 8px solid #d60c47;
+      ul {
+        width: 100%;
+        padding-bottom: 100%;
+        background-color: #ffffff;
+        li {
+          position: absolute;
+          top: 0;
+          width: 100%;
+          height: 100%;
+          background-color: azure;
+        }
+      }
+      .img_nav {
+        position: absolute;
+        width: 100%;
+        height: 0;
+        padding: 0 25px;
+        top: calc(50% - 15px);
+        &:after {
+          content: '';
+          clear: both;
+          display: block;
+        }
+        .prev_button, .next_button {
+          width: 40px;
+          height: 40px;
+          background-color: #f0f0f0;
+          border: 3px solid #ffffff;
+          border-radius: 100%;
+          opacity: 0.5;
+          img {
+            width: 100%;
+            height: 100%;
+            transform: scale(0.8);
+          }
+          &:focus {
+            outline: none;
+          }
+          &:hover {
+            opacity: 1;
+          }
+        }
+        .prev_button {
+          float: left;
+        }
+        .next_button {
+          float: right;
+        }
+      }
+      .img_position {
+        position: absolute;
+        width: 100%;
+        height: 30px;
+        bottom: 5px;
+        text-align: center;
+        p {
+          width: 60px;
+          height: 30px;
+          margin: 0 auto;
+          line-height: 30px;
+          font-size: 25px;
+          color: #fefefe;
+          background-color: rgba(50, 50, 50, 0.5);
+          border-radius: 10px;
+        }
+      }
+    }
+    .img_border {
+      position: relative;
+      width: 50%;
+      padding-bottom: 50%;
+      border-radius: 120%;
+      background-image: linear-gradient(
+          135deg, #002778 25%, #ffffff 25%, #ffffff 50%, #002778 50%, #002778 75%, #ffffff 75%, #ffffff 100%
+      );
+      background-size: 20px 20px;
+      right: -48%;
+      margin-top: 38%;
+      z-index: 300;
+    }
+  }
+
+  .culture_text {
+    width: 35%;
+    float: left;
+    text-align: left;
+    padding-left: 2%;
+    .inner_title {
+      h4 {
+        font-size: 55px;
+        font-weight: bold;
+        margin: 38px 0 3px;
+        color: #d60c47;
+      }
+      p {
+        font-family: 'NanumSquare', sans-serif;
+        font-size: 23px;
+        padding: 0 2px;
+      }
+    }
+    .division_bar_small {
+      width: 50px;
+      height: 2px;
+      background-color: #212121;
+      margin: 8px 4px 15px;
+    }
+    .inner_content {
+      p {
+        font-family: 'NanumSquare', sans-serif;
+        font-size: 14px;
+      }
+    }
+  }
+
+  .side_text {
+    width: 5%;
+    float: left;
+    font-size: 34px;
+    font-weight: bold;
+    padding-top: 45px;
+    color: #000;
+    p {
+      transform: rotate(90deg);
+      word-break: keep-all;
+      white-space: nowrap;
+    }
   }
   /* section 1 end */
 
-  /* section 2 start */
   .section_2 {
-    .section_2_title {
-      color: #fff68f;
-    }
-    .section_2_describe {
-      font-size: 25px;
-    }
-    width: 100%;
-    padding: 50px 20px;
-    background-color: steelblue;
-    color: #efefef;
-    text-align: center;
-    .what_we_doing {
-      width: 100%;
-      max-width: 1250px;
-      margin: 0 auto;
-      .doing_list {
-        display: inline-block;
-        width: 45%;
-        min-width: 340px;
-        max-width: 400px;
-        min-height: 125px;
-        padding: 10px 30px;
-        vertical-align: top;
-        text-align: left;
-        .doing_list_title{
-          font-size: 22px;
-          padding: 10px 0;
-          color: #fff68f;
+    .culture_wrap {
+      &:after {
+        content: '';
+        clear: both;
+        display: block;
+      }
+      .culture_img_wrap {
+        float: right;
+        .culture_img {
+          float: right;
+          border: 8px solid #002778;
         }
-        .doing_ul {
-          padding: 15px 22px;
-          font-size: 14px;
-          text-align: left;
-          color: #d1d1d1;
-          li {
-            list-style: disc;
+        .img_border {
+          background-image: linear-gradient(
+              45deg, #d60c47 25%, #ffffff 25%, #ffffff 50%, #d60c47 50%, #d60c47 75%, #ffffff 75%, #ffffff 100%
+          );
+          right: 0;
+        }
+      }
+      .culture_text {
+        float: right;
+        .inner_title {
+          h4 {
+            color: #002778;
           }
         }
       }
+      .side_text {
+        float: right;
+      }
     }
-  }
-  /* section 2 end */
+  }/* section 2 end */
+
 
   .section_3 {
-    padding: 0;
-    .section_3_boxes {
-      position: relative;
-      text-align: center;
-      background-color: #1c3448;
-      //meet boxes(float left) in it.
-      &:after {
-        content: '';
-        display: block;
-        clear: both;
-      }
-      .box_title {
-        margin: 0;
-        font-size: 15px;
-      }
-      .box_describe {
-        font-size: 25px;
-        margin: 0;
-        padding: 20px 0;
-      }
-      .router_wrap {
-        padding: 20px 0;
-        font-size: 14px;
-      }
-      .router {
-        color: #fff68f;
-        &:hover {
-          color: #efefef;
-        }/* Router hover */
-      }/* Router */
-    }/* Section 3 boxes */
+
   }/* Section 3 ended */
 
-  .meet_box_1 {
-    width: 50%;
-    min-height: 255px;
-    vertical-align: top;
-    float: left;
-    padding: 40px 20px;
-    background-color: #1c3448;
-    color: #efefef;
-  }
-  .meet_box_2 {
-    width: 50%;
-    min-height: 255px;
-    vertical-align: top;
-    float: left;
-    padding: 40px 20px;
-    background-color: #2a4e6c;
-    color: #efefef;
-  }
 
-  @media (max-width: 725px) {
-    .meet_box_1 {
+  /*
+  ===============================================
+  Media query
+  ===============================================
+  */
+  @media (max-width: 835px) {
+    .culture_img_wrap {
       width: 100%;
-      min-height: 255px;
-      vertical-align: top;
-      padding: 40px 20px;
-      background-color: #1c3448;
-      color: #efefef;
     }
-    .meet_box_2 {
+    .culture_text {
       width: 100%;
-      min-height: 255px;
-      vertical-align: top;
-      padding: 40px 20px;
-      background-color: #2a4e6c;
-      color: #efefef;
+    }
+    .side_text {
+      display: none;
     }
   }
 
